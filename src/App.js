c58e16dd02a6c710df4b65296d93591120d192d6
Vicant <josepepevillamil@gmail.com>
2025-04-19 22:11:34 -0600

    Borrando archivos que no sirven

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import './App.css';
-import MyFirstComponent from './components/MyFirstComponent';
-import Form from './components/Form/Form';
-import Profile from './components/Profile';
 import { UserContextProvider } from './context/user-context';
-import { Link } from 'react-router-dom';
-import MyRouters from './router/Router'
 import Menu from './page/base/Menu';
 
 export default function App() {
